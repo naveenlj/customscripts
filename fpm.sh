3694e48765cdf78ebcf91835ca9dc679fde1f6c7
Naveen <naveenlj@users.noreply.github.com>
2016-01-02 11:54:22 +0530

    Create fpm.sh

---
+++ fpm.sh
@@ -0,0 +1,25 @@
+#/bin/bash
+
+set -x
+
+YUM_CMD=$(which yum)
+  APT_GET_CMD=$(which apt-get)
+  #OTHER_CMD=$(which <other installer>)
+
+ if [[ ! -z $YUM_CMD ]]; then
+    yum -y install ruby-devel gcc
+    yum -y install rubygems
+    gem install fpm
+ elif [[ ! -z $APT_GET_CMD ]]; then
+
+apt-get -y update
+apt-get install ruby-dev build-essential
+
+gem install fpm --no-ri --no-rdoc
+
+#elif [[ ! -z $OTHER_CMD ]]; then
+#    $OTHER_CMD <proper arguments>
+# else
+#    echo "error can't install package $PACKAGE"
+#    exit 1;
+ fi
